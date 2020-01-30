3c4b9b4f60117a6406d3b233f02fcec3c3e419f1
abdulmabud <abdulmabudm@gmail.com>
2020-01-30 15:05:05 +0600

    Product model and migration added

---
+++ database/migrations/2020_01_30_072738_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 100);
+            $table->float('regular_price', 8, 2);
+            $table->float('sale_price', 8, 2);
+            $table->unsignedBigInteger('category_id');
+            $table->tinyInteger('status')->default(1);
+            $table->foreign('category_id')->references('id')->on('categories');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
