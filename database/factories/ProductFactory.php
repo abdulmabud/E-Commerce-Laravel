bca5708d4f0e1ec96087fb683882107d413a559a
abdulmabud <abdulmabudm@gmail.com>
2020-01-31 22:30:57 +0600

    Product factory added

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+use App\Product;
+
+
+$factory->define(Product::class, function (Faker $faker) {
+    return [
+        
+            'name' => $faker->name,
+            'regular_price' => random_int(50, 1000),
+            'sale_price' => random_int(50, 1000),
+            'category_id' => 1,
+            'status' => 1
+
+    ];
+});
