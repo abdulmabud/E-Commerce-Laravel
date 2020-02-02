34dcab95f56635d368f4ac3ea5fde94e74d6012a
abdulmabud <abdulmabudm@gmail.com>
2020-02-02 14:00:42 +0600

    OrderItem model and migration added

---
+++ database/migrations/2020_02_02_075023_create_order_items_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_items', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('order_id');
+            $table->string('product_name', 100);
+            $table->float('unit_price', 8, 2);
+            $table->integer('quantity');
+            $table->timestamps();
+            $table->foreign('order_id')->references('id')->on('orders');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_items');
+    }
+}
