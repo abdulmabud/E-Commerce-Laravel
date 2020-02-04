b26b6a41ef60a83907a7a07d0e205fcaa2df0132
abdulmabud <abdulmabudm@gmail.com>
2020-02-05 00:01:29 +0600

    Add user_phone, user_email column

--- database/migrations/2020_02_02_073501_create_orders_table.php
+++ database/migrations/2020_02_02_073501_create_orders_table.php
@@ -16,7 +16,9 @@ class CreateOrdersTable extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->unsignedBigInteger('user_id')->nullable();
-            $table->string('user_name');
+            $table->string('user_name', 100);
+            $table->string('user_phone', 50);
+            $table->string('user_email', 100);
             $table->string('address', 300);
             $table->float('subtotal', 8, 2);
             $table->float('delivery_charge', 8, 2);
