e461b35054f6d61c42cde64ad19baec8cd49ef34
abdulmabud <abdulmabudm@gmail.com>
2020-07-21 16:58:36 +0600

    Is_admin column added in users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,6 +21,7 @@ class CreateUsersTable extends Migration
             $table->string('address');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('is_admin')->default(false);
             $table->rememberToken();
             $table->timestamps();
         });
