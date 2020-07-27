abfec7381c97f05fe08a228990717d721efcc980
abdulmabud <abdulmabudm@gmail.com>
2020-07-27 11:47:59 +0600

    Make homepage slider full screen

--- resources/views/Frontend/index.blade.php
+++ resources/views/Frontend/index.blade.php
@@ -5,38 +5,37 @@
 @endsection
 
 @section('content')
+    <div class="slider" style="margin-top: -25px;">
+      <div id="carouselExampleIndicators" class="carousel slide my-4" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner" role="listbox">
+          @foreach ($slider_images as $image)
+        <div class="carousel-item {{ $active == 1 ? 'active':'' }}">
+            <img class="d-block img-fluid" src="{{ asset('upload/slider/'.$image->meta_value) }}" width="100%" alt="First slide">
+          </div>
+          @php
+              $active = 2;
+          @endphp
+          @endforeach
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+    </div>
 <div class="container">
 
     <div class="row">
-
-
       <div class="col-lg-12">
-
-        <div id="carouselExampleIndicators" class="carousel slide my-4" data-ride="carousel">
-          <ol class="carousel-indicators">
-            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-          </ol>
-          <div class="carousel-inner" role="listbox">
-            @foreach ($slider_images as $image)
-          <div class="carousel-item {{ $active == 1 ? 'active':'' }}">
-              <img class="d-block img-fluid" src="{{ asset('upload/slider/'.$image->meta_value) }}" alt="First slide">
-            </div>
-            @php
-                $active = 2;
-            @endphp
-            @endforeach
-          </div>
-          <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-          </a>
-          <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-          </a>
-        </div>
         @php
             if($fproducts->count() != 0){
               echo '<h2>Featured Product</h2>';
