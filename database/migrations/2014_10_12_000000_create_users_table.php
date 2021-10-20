8aef9604adc43b8a31f1b359a7a9642b4d18bc00
Abdul Mabud <abdulmabudm@gmail.com>
2021-10-20 10:29:43 +0600

    Update users table migration and user model

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,8 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('phone', 50);
+            $table->string('address', 150);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
